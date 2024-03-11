7428ec29e4860d2e0481d79e52e96be906986e3f
Shruti <Shruti@LAPTOP-LI0QVQGC>
2024-03-11 17:36:00 +0530

    rebase main

---
+++ src/Practice/Even_Number.java
@@ -0,0 +1,22 @@
+package Practice;
+
+
+public class Even_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=11;
+		if(a%2==0)
+		{
+			System.out.println(a+" is even number");
+		}else
+		{
+			System.out.println(a+" is odd number");
+		}
+
+	}
+
+}
+
